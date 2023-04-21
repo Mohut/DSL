09dfeb5981873ebd8bfafdfae49e1f60a2ade2c3
Mohut <dean.celle00@gmail.com>
2023-04-21 10:13:04 +0200

    made subject menu dynamic

--- DSL/Assets/Scripts/Screens/Subject Screen/SubjectButton.cs
+++ DSL/Assets/Scripts/Screens/Subject Screen/SubjectButton.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using Mono.Cecil.Cil;
 using TMPro;
+using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,40 +9,41 @@ using UnityEngine.UI;
 public class SubjectButton : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI inputField;
+    [SerializeField] private GameObject subjectButtonPrefab;
+    [SerializeField] private Transform parentTransform;
     
     [Header("Buttons")]
-    [SerializeField] private List<Button> buttons;
     [SerializeField] private List<TextMeshProUGUI> buttonTexts;
     [SerializeField] private Button homebutton;
 
     private void Start()
     {
-        SetUpButtons();
+        CreateSubjectButtons();
         homebutton.onClick.AddListener(SceneManager.LoadMainMenu);
     }
 
-    private void SetUpButtons()
+    private void CreateSubjectButtons()
     {
+        int yPosition = -25;
+
         for (int i = 0; i < DataManager.Instance.Stations.Count; i++)
         {
-            buttons[i].gameObject.SetActive(true);
-            buttonTexts[i].SetText(DataManager.Instance.Stations[i].name);
-            Station buttonStation = DataManager.Instance.Stations[i];
-            buttons[i].onClick.AddListener(() =>
+            Vector3 buttonPosition = new Vector3(0, yPosition, 0);
+            
+            GameObject subjectButton = Instantiate(subjectButtonPrefab, parentTransform.position, quaternion.identity);
+            subjectButton.transform.parent = parentTransform;
+            subjectButton.GetComponent<SubjectButtonInterface>().RectTransform.localPosition = buttonPosition;
+            subjectButton.GetComponent<SubjectButtonInterface>().SubjectTitleTMP.text = DataManager.Instance.Stations[i].name;
+            
+            int stationIndex = i;
+            subjectButton.GetComponent<Button>().onClick.AddListener(() =>
             {
                 DataManager.Instance.AddNewGroup(inputField.text);
-                GameManager.Instance.SetCurrentStation(buttonStation);
+                GameManager.Instance.SetCurrentStation(DataManager.Instance.Stations[stationIndex]);
                 SceneManager.LoadQuestionScreen();
             });
-        }
-    }
 
-    private void OnDisable()
-    {
-        foreach (Button button in buttons)
-        {
-            button.onClick.RemoveAllListeners();
+            yPosition -= 150;
         }
-        homebutton.onClick.RemoveAllListeners();
     }
 }
